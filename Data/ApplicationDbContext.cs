37f4a621a2f7ef6a0d6d5ede171d79ea6ebeaaad
brandonroberts <brandonroberts@BRANDONROBEDFA1>
2021-08-22 12:04:48 -0400

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AngularAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Movie> movies { get; set; } 
+    }
+}
---
+++ Models/Movies.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AngularAPI.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Year { get; set; }
+    }
+}
